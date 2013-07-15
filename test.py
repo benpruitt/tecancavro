83bea6e6aa6fc46d6ee402463bd24931e70785eb
ben <bpruittvt@gmail.com>
2013-07-15 18:42:29 -0400

    updated test.py

--- test.py
+++ test.py
@@ -2,11 +2,12 @@ from tecancavro.models import XCaliburD
 
 from tecancavro.transport import TecanAPISerial, TecanAPINode
 
+# Functions to return instantiated XCaliburD objects for testing
 
-def returnCavro():
-    testO = XCaliburD(com_link=TecanAPISerial(0, '/dev/ttyUSB0', 9600))
-    return testO
+def returnSerialXCaliburD():
+    test0 = XCaliburD(com_link=TecanAPISerial(0, '/dev/ttyUSB0', 9600))
+    return test0
 
-def httpTest():
+def returnNodeXCaliburD():
 	test0 = XCaliburD(com_link=TecanAPINode(0, '192.168.1.140:80'), waste_port=6)
 	return test0
